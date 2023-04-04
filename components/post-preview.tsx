6851ee0a2555fd2f99cee0ca629448ca25adf09e
Adrian Hong <adrian.hong@mongodb.com>
2023-04-05 09:11:52 +1000

    feat: Post styling

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,17 +1,17 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import type Author from '../interfaces/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+import type Author from "../interfaces/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+};
 
 const PostPreview = ({
   title,
@@ -22,26 +22,24 @@ const PostPreview = ({
   slug,
 }: Props) => {
   return (
-    <div>
-      <div className="mb-5">
+    <Link
+      as={`/posts/${slug}`}
+      href="/posts/[slug]"
+      className="border-2 rounded-xl border-black hover:bg-blue-300"
+    >
+      <div className="p-4">
+        {/* <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
+      </div> */}
+        <h3 className="text-3xl mb-3 leading-snug">{title}</h3>
+        <div className="text-lg mb-4">
+          <DateFormatter dateString={date} />
+        </div>
+        <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        <Avatar name={author.name} picture={author.picture} />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
-          {title}
-        </Link>
-      </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
-    </div>
-  )
-}
+    </Link>
+  );
+};
 
-export default PostPreview
+export default PostPreview;
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,14 @@ type Props = {
 };
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  // const heroPost = allPosts[0];
+  const morePosts = allPosts;
   return (
     <>
       <Layout>
         <Container>
           <Intro />
-          {heroPost && (
+          {/* {heroPost && (
             <HeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
@@ -29,7 +29,7 @@ export default function Index({ allPosts }: Props) {
               slug={heroPost.slug}
               excerpt={heroPost.excerpt}
             />
-          )}
+          )} */}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
