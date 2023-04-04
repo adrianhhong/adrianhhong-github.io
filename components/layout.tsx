ebe0e8f2d0900daaf18027950da579ed71bd7188
Adrian Hong <adrian.hong@mongodb.com>
2023-04-05 08:30:32 +1000

    feat: Changed Head

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,23 +1,27 @@
-import Alert from './alert'
-import Footer from './footer'
-import Meta from './meta'
+import Head from "next/head";
+import Alert from "./alert";
+import Footer from "./footer";
+import Meta from "./meta";
+import { WEBSITE_TITLE } from "../lib/constants";
 
 type Props = {
-  preview?: boolean
-  children: React.ReactNode
-}
+  preview?: boolean;
+  children: React.ReactNode;
+};
 
 const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
+      <Head>
+        <title>{WEBSITE_TITLE}</title>
+      </Head>
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
