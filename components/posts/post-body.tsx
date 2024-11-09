86eaebf8e3cbf747061050d82d73701822c93da0
Adrian Hong <adrian.hong@mongodb.com>
2024-11-09 23:34:31 +1100

    fix: Remove reference to next/image since static site

--- components/posts/post-body.tsx
+++ components/posts/post-body.tsx
@@ -1,33 +1,18 @@
 import markdownStyles from "../../styles/markdown-styles.module.css";
 import Markdown from "react-markdown";
-import Image from "next/image";
 import rehypeCodeTitles from "rehype-code-titles";
 import rehypePrismAll from "rehype-prism-plus/all";
 
 type Props = {
   content: string;
-  imageSizes: Record<string, { width: number; height: number }>;
 };
 
-const PostBody = ({ content, imageSizes }: Props) => {
+const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-5xl mx-auto">
       <Markdown
         className={markdownStyles["markdown"]}
         components={{
-          img: (props) => {
-            if (imageSizes[props.src]) {
-              const { src, alt } = props;
-              const { width, height } = imageSizes[props.src];
-              return (
-                <Image src={src} alt={alt} width={width} height={height} />
-              );
-            } else {
-              // If we don’t have the image’s dimensions, let’s use a classic
-              // `img` element.
-              return <img {...props} />;
-            }
-          },
           a: (props) => (
             <a href={props.href} target="_blank" rel="noreferrer">
               {props.children}
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,17 +7,14 @@ import Layout from "../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../lib/api";
 import PostTitle from "../../components/posts/post-title";
 import type PostType from "../../interfaces/post";
-import sizeOf from "image-size";
-import { join } from "path";
 
 type Props = {
   post: PostType;
   morePosts: PostType[];
   preview?: boolean;
-  imageSizes: Record<string, { width: number; height: number }>;
 };
 
-export default function Post({ post, morePosts, preview, imageSizes }: Props) {
+export default function Post({ post, morePosts, preview }: Props) {
   const router = useRouter();
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
@@ -35,7 +32,7 @@ export default function Post({ post, morePosts, preview, imageSizes }: Props) {
                 date={post.date}
                 tags={post.tags}
               />
-              <PostBody content={post.content} imageSizes={imageSizes} />
+              <PostBody content={post.content} />
             </article>
           </>
         )}
@@ -62,26 +59,9 @@ export async function getStaticProps({ params }: Params) {
     "tags",
   ]);
 
-  const imageSizes: Props["imageSizes"] = {};
-
-  // A regular expression to iterate on all images in the post
-  const iterator = post.content.matchAll(/\!\[.*]\((.*)\)/g);
-  let match: IteratorResult<RegExpMatchArray, any>;
-  while (!(match = iterator.next()).done) {
-    const [, src] = match.value;
-    try {
-      // Images are stored in `public`
-      const { width, height } = sizeOf(join("public", src));
-      imageSizes[src] = { width, height };
-    } catch (err) {
-      console.error(`Can’t get dimensions for ${src}:`, err);
-    }
-  }
-
   return {
     props: {
       post,
-      imageSizes,
     },
   };
 }
